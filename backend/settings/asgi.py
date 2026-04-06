a2a82bf8a485697c83621607432ee2bdc7082abd
satbaevali <156926704+satbaevali@users.noreply.github.com>
2026-04-07 01:06:10 +0500

    temp changes

--- backend/settings/asgi.py
+++ backend/settings/asgi.py
@@ -4,6 +4,13 @@ import os
 # Project modules
 from settings.conf import BLOG_ENV_ID, ENV_ID_POSSIBLE_OPTIONS
 from django.core.asgi import get_asgi_application
+from apps.notifications.routing import websocket_urlpatterns
+
+#third party modules
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+
+
 
 
 assert BLOG_ENV_ID in ENV_ID_POSSIBLE_OPTIONS, (
@@ -12,4 +19,15 @@ assert BLOG_ENV_ID in ENV_ID_POSSIBLE_OPTIONS, (
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.base")
 
-application = get_asgi_application()
+django_asgi_app = get_asgi_application()
+
+
+application = ProtocolTypeRouter(
+    {
+        "http": django_asgi_app,
+        "websocket": AllowedHostsOriginValidator(
+            URLRouter(websocket_urlpatterns)
+        ),
+    }
+)
+
